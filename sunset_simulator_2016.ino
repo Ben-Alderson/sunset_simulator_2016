65858298e0e93a8cee4536799c4bc011d6296612
Ben-Alderson <bennyalderson@gmail.com>
2016-09-12 13:34:43 -0400

    Initial Commit

---
+++ sunset_simulator_2016.ino
@@ -0,0 +1,15 @@
+int red = 7;
+int green = 5;
+int blue = 6;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, LOW);
+}
